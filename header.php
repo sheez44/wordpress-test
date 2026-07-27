8648f6b93388986ff1519fe0ac67ece6b4ca442d
Psydev <blaat2k@mail.com>
2026-07-27 14:07:19 +0200

    add body_class and wp_body_open

--- header.php
+++ header.php
@@ -12,5 +12,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body>
-    <header>Header</header>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<header>Header</header>
