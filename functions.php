12bb6807356ea1f2ed4154f74a95f9dab3c246f1
Psydev <blaat2k@mail.com>
2026-07-27 14:30:24 +0200

    add initial enqueue style + scripts

--- functions.php
+++ functions.php
@@ -4,3 +4,13 @@
  * 
  * @package Test
  */
+
+
+function test_enqueue_scripts() {
+    wp_enqueue_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css') );
+    wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true );
+}
+
+add_action('wp_enqueue_scripts', 'test_enqueue_scripts');
+
+?>
