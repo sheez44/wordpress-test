a8e901d948c374a59003b098a9ba2f7c168e0718
Psydev <blaat2k@mail.com>
2026-07-28 09:41:47 +0200

    add bootstrap 5

--- functions.php
+++ functions.php
@@ -7,8 +7,14 @@
 
 
 function test_enqueue_scripts() {
+    // styles
     wp_enqueue_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css') );
+    wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/assets/vendor/bootstrap/css/bootstrap.css');
+
+    // scripts
     wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true );
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/assets/vendor/bootstrap/js/bootstrap.js', ['jquery']);
+
 }
 
 add_action('wp_enqueue_scripts', 'test_enqueue_scripts');
