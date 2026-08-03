4cb1ca8c1a0c7cf07e9522d14082d0a69a0ee41f
Psydev <blaat2k@mail.com>
2026-08-03 13:53:50 +0200

    add display taxonomy terms

--- template-parts/blog/footer.php
+++ template-parts/blog/footer.php
@@ -4,4 +4,21 @@
  * 
  * @package Test
  */
+
+$the_post_id = get_the_ID();
+$taxonomy_terms = wp_get_post_terms( $the_post_id, ['category', 'post_tag']);
+
+if(empty($taxonomy_terms) || !is_array($taxonomy_terms)) {
+    return;
+}
 ?>
+
+<div class="entry-footer mt-4">
+    <?php foreach($taxonomy_terms as $term): ?>
+        <?php if($term->name != 'Uncategorized' ): ?>
+        <a class="btn border border-secondary" href="<?= esc_url(get_term_link($term)); ?>">
+            <?= $term->name; ?>
+        </a>
+        <?php endif;?>
+    <?php endforeach; ?>
+</div>
